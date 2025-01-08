dc3bb9c375664ee654145778a374b5d12f3d357f
Abel Embaye <aembaye@uark.edu>
2025-01-07 19:02:30 -0600

    committed on 1/7/2025c

--- components/canvas/lib/text.ts
+++ components/canvas/lib/text.ts
@@ -1,22 +1,22 @@
-import { fabric } from "fabric"
-import FabricTool, { ConfigureCanvasProps } from "./fabrictool"
+import { fabric } from "fabric";
+import FabricTool, { ConfigureCanvasProps } from "./fabrictool";
 
 class TextTool extends FabricTool {
   configureCanvas({}: ConfigureCanvasProps): () => void {
-    this._canvas.isDrawingMode = false
-    this._canvas.selection = false
-    this._canvas.forEachObject((o) => (o.selectable = o.evented = false))
+    this._canvas.isDrawingMode = false;
+    this._canvas.selection = false;
+    this._canvas.forEachObject((o) => (o.selectable = o.evented = false));
 
-    this._canvas.on("mouse:down", (e: any) => this.onMouseDown(e))
+    this._canvas.on("mouse:down", (e: any) => this.onMouseDown(e));
     return () => {
-      this._canvas.off("mouse:down")
-    }
+      this._canvas.off("mouse:down");
+    };
   }
 
   onMouseDown(o: any) {
-    let canvas = this._canvas
-    let _clicked = o.e["button"]
-    var pointer = canvas.getPointer(o.e)
+    let canvas = this._canvas;
+    let _clicked = o.e["button"];
+    var pointer = canvas.getPointer(o.e);
 
     if (_clicked === 0) {
       let text = new fabric.IText("", {
@@ -25,13 +25,15 @@ class TextTool extends FabricTool {
         fontSize: 25,
         selectable: true,
         evented: true,
-      })
-      canvas.add(text)
-      canvas.setActiveObject(text)
-      text.enterEditing()
-      text.hiddenTextarea.focus()
+      });
+      canvas.add(text);
+      canvas.setActiveObject(text);
+      text.enterEditing();
+      if (text.hiddenTextarea) {
+        text.hiddenTextarea.focus();
+      }
     }
   }
 }
 
-export default TextTool
+export default TextTool;
